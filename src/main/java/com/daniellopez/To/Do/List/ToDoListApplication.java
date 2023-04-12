d4fcf8227758a4e88f9b1c5d071ea48f2227816d
dlopez1591 <dlopez1591@gmail.com>
2023-04-11 21:28:20 -0500

    init Data Updated

--- src/main/java/com/daniellopez/To/Do/List/ToDoListApplication.java
+++ src/main/java/com/daniellopez/To/Do/List/ToDoListApplication.java
@@ -1,13 +1,42 @@
 package com.daniellopez.To.Do.List;
 
+import com.daniellopez.To.Do.List.models.Client;
+import com.daniellopez.To.Do.List.models.Task;
+import com.daniellopez.To.Do.List.models.taskStatus;
+import com.daniellopez.To.Do.List.repositories.ClientRepository;
+import com.daniellopez.To.Do.List.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class ToDoListApplication {
-
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoListApplication.class, args);
 	}
-
+	/* es una anotación que le indica a Spring que cree y administre un objeto en tu aplicación (Esto significa que Spring se encarga de la creación, configuración y destrucción de la instancia del objeto,
+	 lo que te permite concentrarte en el código de tu aplicación y no preocuparte por la gestión de los objetos.)
+	 */
+	@Bean
+	public CommandLineRunner initData(ClientRepository clientRepository, TaskRepository taskRepository) {
+		return (args) -> {
+			Client daniel = new Client("Daniel", "Lopez", passwordEncoder.encode("daniel"), "dlopez1591@gmail.com");
+			Client yenny = new Client ("Yenny", "Hernandez", passwordEncoder.encode("yenny"),  "yenny123@gmail.com");
+			Task tareaUno = new Task("Tarea Uno", "Crear una tarea", LocalDate.now(), taskStatus.Comenzada, daniel);
+			Task tareaDos = new Task("Tarea Dos", "Crear otra tarea", LocalDate.now(), taskStatus.Desarrollando, yenny);
+			daniel.addTask(tareaUno);
+			yenny.addTask(tareaDos);
+			clientRepository.save(daniel);
+			clientRepository.save(yenny);
+			taskRepository.save(tareaUno);
+			taskRepository.save(tareaDos);
+		};
+	}
 }
