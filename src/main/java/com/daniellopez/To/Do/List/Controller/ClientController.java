e0b4a4da5f4bbe07913f8d4e8a4fd28b52c8f07f
dlopez1591 <dlopez1591@gmail.com>
2023-04-12 20:48:09 -0500

    Metodo Cliente autenticado por ID

--- src/main/java/com/daniellopez/To/Do/List/Controller/ClientController.java
+++ src/main/java/com/daniellopez/To/Do/List/Controller/ClientController.java
@@ -6,9 +6,12 @@ import com.daniellopez.To.Do.List.repositories.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+import java.util.Optional;
+
 import static java.util.stream.Collectors.toList;
 
 //con este requestmapping, indicamos que todas las rutas decritas van a ser /api
@@ -22,15 +25,19 @@ public class ClientController {
     @Autowired
     private PasswordEncoder passwordEncoder;
     //ahora vamos a definir un metodo que retorne el listado de clientes
-    @RequestMapping("clients/current")
-    public ClientDTO getCurrentClient(Authentication authentication) {
-        return new ClientDTO(clientRepository.findByEmail(authentication.getName()));
-    }
     @RequestMapping("/clients")
     public List<ClientDTO> getClients(){
         return clientRepository.findAll().stream().map(client -> new ClientDTO(client)).collect(toList());
         //clientRepository.findAll().stream().map(ClientDTO::new).collect(toList());
     }
     //Metodo para tener informacion del cliente autenticado
-
+    @RequestMapping("clients/current")
+    public ClientDTO getCurrentClient(Authentication authentication) {
+        return new ClientDTO(clientRepository.findByEmail(authentication.getName()));
+    }
+    //Metodo para obtener un cliente por su ID
+    @RequestMapping("/clients/{id}")
+    public Optional<ClientDTO> getClient(@PathVariable Long id){
+        return clientRepository.findById(id).map(client -> new ClientDTO(client));
+    }
 }
