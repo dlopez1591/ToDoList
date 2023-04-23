2cb2625b3a3e56ee5817f01b2b8971d18f364f21
dlopez1591 <dlopez1591@gmail.com>
2023-04-22 22:05:50 -0500

    maqueteando un nuevo index

--- src/main/resources/static/web/assets/scripts/index.js
+++ src/main/resources/static/web/assets/scripts/index.js
@@ -1,50 +1,51 @@
-
-
 const { createApp } = Vue
+
 createApp({
-    data() {
-      return {
-        email: '',
-        password: ''
-      }
-    },
-    methods:{
-        login(){
-          axios.post('/api/login', `email=${this.email}&password=${this.password}`, {
+  data() {
+    return {
+      email: '',
+      password: '',
+      firstName: '',
+      lastName: ''
+    }
+  },
+  methods: {
+    login() {
+      axios
+        .post(
+          '/api/login',
+          { email: this.email, password: this.password },
+          {
             headers: {
               'Content-Type': 'application/x-www-form-urlencoded'
-          }
-          })
-          .then(response => {
-            Swal.fire({
-              position: 'midle',
-              icon: 'success',
-              color: 'white',
-              background: '#000000c1',
-              text: 'Your login have been successful',
-              showConfirmButton: false,
-            })
-            .then(()=> {
-              if (this.email == "admin@admin.com"){
-                window.location.href ='web/admin.html';
-              }
-              else{
-                window.location.href = 'web/manageTime.html'
-              }
             }
-            )
           }
-          )
-          .catch(error => {
-            this.error = error.response.data.message;
-            Swal.fire({
-                icon: 'error',
-                color: 'white',
-                background: '#B22222',
-                text: 'Verifica la informacion que ingresaste',
-            })
-        });
-
-        }
+        )
+        .then((response) => {
+          Swal.fire({
+            position: 'middle',
+            icon: 'success',
+            background: '#000000c1',
+            textColor: 'white',
+            text: 'Your login has been successful',
+            showConfirmButton: false
+          }).then(() => {
+            if (this.email === 'admin@admin.com') {
+              window.location.href = 'web/admin.html'
+            } else {
+              window.location.href = 'web/manageTime.html'
+            }
+          })
+        })
+        .catch((error) => {
+          this.error = error.response.data.message
+          Swal.fire({
+            icon: 'error',
+            background: '#B22222',
+            textColor: 'white',
+            text: 'Verify the information you entered'
+          })
+        })
     }
-  }).mount('#app')
+  }
+}).mount('#app')
